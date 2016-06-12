71e1035c7933a748746ab2caa7fc343bfe92c7f7
ancalotoru <ancalotoru@gmail.com>
2016-06-12 10:44:35 +0200

    Plackett-Luce distribution model added. EHBSA distribution model added. Bayesian Network corrections made.

--- R/BayesianNetwork.R
+++ R/BayesianNetwork.R
@@ -1,8 +1,6 @@
-#' An S4 class to represent distributions based on univariate marginals
+#' An S4 class to represent distributions based on Bayesian networks
 #'
-#' @slot population The population where the network is based on
-#' @slot network An object of the type bn (see BNLearn pacakge for details)
-#' @slot levels A list of levels in case the model is constructed over factors. If it is to be used with logical values, then this slot will be \code{NULL}.
+#' @slot network An object of the type bn.fit (see bnLearn pacakge for details)
 #' 
 
 # We import bnLearn Package
@@ -10,89 +8,106 @@ require(bnlearn)
 require(gRain)
 setClass(
 	Class="BayesianNetwork", 
-	representation=representation(network="ANY")
+	representation=representation(network="bn.fit")
 	# slots = c(network="CPTgrain", numColumns="integer")
 	# representation=representation(prob.table="matrix", 
 	#                               binary="logical")
 )
 
 
-#REPASAR LA VALIDACION
-#setValidity(
-#   Class="BayesianNetwork", 
-#   method=function(object) {
-#     m <- object@network
-#     if (!all(object@network <= 1 & object@network >= 0)) {
-#       stop("All the values in the prob table have to be between 0 and 1")
-#     }
-		
-#     if (object@binary) {
-#       if (nrow(object@prob.table) != 1) {
-#         stop("For models over logical variables the 'prob.table' should only ",
-#              "contain one row, representing the probability of the variable being 'TRUE'")
-#       }
-#     } else {
-#       if (is.null(rownames(object@prob.table))) {
-#         stop("For factor models the rows in the probability table have to be named ",
-#              "with the level values")
-#       }
-#       if (nrow(object@prob.table) < 2) {
-#         stop("For factor models there should be at least two possible values")
-#       }
-#       # Note that, due to rounding problems, the summ may not be exactly 1
-#       if (max(colSums(object@prob.table) - 1) > 1 / (100 * nrow(object@prob.table))) {
-#         stop("The colºumn-wise sums in 'prob.table' should be 1")
-#       }
-#     }
-#     return (TRUE)
-#})
+setValidity(
+   Class="BayesianNetwork", 
+   method=function(object) {
+     return (TRUE)
+})
 
 
 # GENERIC METHODS ---------------------------------------------------------
 setMethod(
 	f="simulate", 
 	signature="BayesianNetwork", 
-	definition=function(object, nsim, ...) {      
-		return(cat2bin(simulate(object@network, nsim = nsim)))
+	definition=function(object, nsim=1, seed=NULL, ...) {
+	  #print("SIMULATE START")
+	  if(!is.null(seed)) {
+	    set.seed(seed)
+	  }
+	  aux <- rbn(object@network, nsim, debug=F)
+	  aux <- as.list(as.data.frame(t(aux)))
+	  aux <- lapply(aux, FUN=cat2bin)
+	  for(i in 1:length(aux)){
+	    for(j in 1:length(aux[[i]])){
+	      if(is.na(aux[[i]][j])){
+	        aux[[i]][j] <- runif(1) >= 0.5
+	      }
+	    }
+	  }
+	  return(aux)
 	})
 
 # CONSTRUCTOR -------------------------------------------------------------
 
-#' Bais consturctor of flip neighborhoods
+#' Constructor of EBNA model
 #' 
-#' This function creates an object of class \code{\linkS4class{FlipNeighborhood}}
+#' This function creates an object of class \code{\linkS4class{BayesianNetowork}}
 #' 
 #' @family EDA
-#' @param data The dataframe containing the initial-population to contruct the bayesian network
+#' @param data The dataframe containing the initial-population to construct the bayesian network
 #' @param ... Ignored
 #' @return An object of class \code{\linkS4class{BayesianNetwork}} that includes the Bayesian Network of the given data
 #' 
-bayesianNetwork <- function(data) {
-	if(class(data) == "list"){  
-		# We can create the object
-		data <- data.frame(data)
-	  aux <- lapply(data, FUN=bin2cat)
-		pop.cat.df <- data.frame(aux)
-		names(pop.cat.df) <- paste("X", 1:length(pop.cat.df), sep = "")
+bayesianNetwork <- function(data, ...) {
+	if(class(data) == "list"){
+	  if(class(data[[1]]) == "logical"){
+	    aux <- lapply(data, FUN=bin2cat)
+	    pop.cat.df <- data.frame(t(matrix(unlist(aux), nrow = length(aux[[1]]), byrow = F)), stringsAsFactors = T)
+	    names(pop.cat.df) <- paste("X", 1:length(pop.cat.df), sep = "")
+	    for(i in 1:length(pop.cat.df)){
+	      pop.cat.df[[i]] <- factor(x=pop.cat.df[[i]], levels=c("T","F"))
+	    }
+	  }else if(class(data[[1]]) == "factor"){
+	    pop.cat.df <- data.frame(t(matrix(unlist(data), nrow = length(data[[1]]), byrow=F)), stringsAsFactors = T)
+	  }
 		network <- hc(x = pop.cat.df)
-		adjacencyMatrix <- amat(network)
-		#n <- ncol(adjacencyMatrix)
-		aux <- as(adjacencyMatrix, "graphNEL")
-		network <- grain(x = aux, data = pop.cat.df, smooth = 1)
+		network <- bn.fit(network, data=pop.cat.df)
 		obj <- new("BayesianNetwork", network = network)
-		return(obj)
 	}else{
 		stop("The data must be a list")
 	}
 }
 
+#' Converts a vector from categorical to logical.
+#' @param catVector categorical vector to be converted.
+#' @return a logical vector
 cat2bin <- function(catVector){
   return(catVector == "T")
 }
 
+#' Converts a vector from logical to categorical.
+#' @param bin.v logical vector to be converted.
+#' @return a categorical vector
 bin2cat <- function(bin.v){
   chr.v <- rep("F", length(bin.v))
   chr.v[bin.v] <- "T"
   catVector <- factor(chr.v, levels=c("T", "F"))
   return(catVector)
+}
+
+#' Calculates the probabilities of the given model for the especified population.
+#' @param object object of the class BayesianNetwork.
+#' @param data population of individuals whoose probabilities are desired to know.
+#' @return a vector containing the probabilities.
+#' 
+calc.prob <- function(object, data){
+  if(class(object) == "BayesianNetwork"){
+    aux <- lapply(data, FUN=bin2cat)
+    pop.cat.df <- data.frame(t(matrix(unlist(aux), nrow = length(aux[[1]]), byrow = F)), stringsAsFactors = T)
+    names(pop.cat.df) <- paste("X", 1:length(pop.cat.df), sep = "")
+    for(i in 1:length(pop.cat.df)){
+      pop.cat.df[[i]] <- factor(x=pop.cat.df[[i]], levels=c("T","F"))
+    }
+    logverosimilitud <- logLik(object@network, pop.cat.df, by.sample=TRUE)
+    return(exp(logverosimilitud))
+  }else{
+    stop("The model must be of the class BayesianNetwork")
+  }
 }
